6929f454d09923ebc43cdf0d42926168a38f4f1e
ty-v1 <fmtyd509@gmail.com>
2024-04-23 12:20:50 +0900

    add entry point

---
+++ client-entry.tsx
@@ -0,0 +1,29 @@
+// @ts-ignore
+import remarkHint from 'remark-hint';
+
+
+declare const growiFacade: any;
+
+const activate = (): void => {
+  if (growiFacade == null || growiFacade.markdownRenderer == null) {
+    return;
+  }
+
+  const { optionsGenerators } = growiFacade.markdownRenderer;
+
+  optionsGenerators.customGenerateViewOptions = (...args: any[]) => {
+    // @ts-ignore
+    const options = optionsGenerators.generateViewOptions(...args);
+    options.remarkPlugins.push(remarkHint)
+
+    return options;
+  };
+};
+
+if ((window as any).pluginActivators == null) {
+  (window as any).pluginActivators = {};
+}
+(window as any).pluginActivators['growi-hint-plugin'] = {
+  activate,
+  deactivate: () => {},
+};
